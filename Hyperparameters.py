36c313d9f3a368e3d9a4238011b92a47c416bbe6
aduk4u <33870014+aduk4u@users.noreply.github.com>
2021-03-20 00:59:47 +0800

    Update Hyperparameters.py

--- Hyperparameters.py
+++ Hyperparameters.py
@@ -1,1 +1,29 @@
 Hyperparameters
+
+
+EPOCHS = 100
+
+BATCH_SIZE = 90
+LearningRate = 0.0001
+#LR_decay = 0.90
+LR_decay = 0.002
+LAM_RECON = 0.392
+ROUTING = 3
+SHIFT_FRACTION = 0.003 ##0.003
+MODEL_PATH = 'models/model-2d.h5'
+#MODEL_PATHN = 'CIFAR10-models/model-2d.h5'
+NUM_CLASSES = 10
+DIGIT_CAP_DIM = 16
+DIGIT_CAP_DIM2 = 16
+n_instance = 7
+n_part=5
+dim_geom=6
+
+# if you want to use Plotly
+# get your id and API-key(it is free)
+PLOTLY_USR = 'your id'
+PLOTLY_API = 'your api-key'
+
+#============ADDED BY ME TO SAVE CSV====================
+save_dir = 'models'
+#=======================================================
